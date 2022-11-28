29be0b0993a2ac8ee96845cd03dda3481dd3b32d
MitchelMA <32548@ma-web.nl>
2022-11-28 10:32:09 +0100

    Default Camera and camera script

--- Assets/Scripts/Utils/Vector3Extensions.cs
+++ Assets/Scripts/Utils/Vector3Extensions.cs
@@ -3,13 +3,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public static class Vector3Extension
+public static class Vector3Extensions
 {
    /// <summary>
    /// Create a Vector3 from the angles Theta and Phi.
    /// Where Theta is the angle from the x-axis to the y-axis and
    /// Phi the angle from the xy-plane to the z-axis.
    /// </summary>
+   /// <param name="r">Length of Vector</param>
    /// <param name="phi">Angle in radians</param>
    /// <param name="theta">Angle in radians</param>
    /// <returns>The corresponding Vector3 of the given angles</returns>
