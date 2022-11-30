c2958442d44359cf02f9f12971e10e91b5f4675b
stokbrootje <leonkruijer@outlook.com>
2022-11-30 10:36:17 +0100

    ChargeDodgeball

--- Assets/Scripts/shootDodgeball.cs
+++ Assets/Scripts/shootDodgeball.cs
@@ -5,10 +5,15 @@ using UnityEngine;
 public class shootDodgeball : MonoBehaviour
 {
     public float speed;
-    public GameObject[] Targets;
-    public int TargetIndex = 0;
     public int dodgeballs = 1;
+    public int TargetIndex = 0;
+    public float ChargeTimer;
+
+    public GameObject[] Targets;
+    public KeyCode ChargeDodgeball;
+    
     public Dodgeball dodgeball;
+    public Dodgeball ChargedDodgeball;
 
     // Start is called before the first frame update
     void Start()
@@ -19,13 +24,24 @@ public class shootDodgeball : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonDown(0) && dodgeballs > 0)
+        if (Input.GetKey(ChargeDodgeball) && dodgeballs > 0)
+        {
+            ChargeTimer += Time.deltaTime;
+        }
+        if (Input.GetKeyUp(ChargeDodgeball) && dodgeballs > 0 && ChargeTimer > 2)
         {
             Debug.Log("shoot");
             Vector3 dir = Targets[TargetIndex].transform.position - transform.position;
+            var clone = Instantiate(ChargedDodgeball);
+            clone.Setup(dir, transform.position, speed * 2);
+
+            dodgeballs -= 1;
+        }
+        if (Input.GetKeyUp(ChargeDodgeball) && dodgeballs > 0 && dodgeballs < 2)
+        {
+            Vector3 dir = Targets[TargetIndex].transform.position - transform.position;
             var clone = Instantiate(dodgeball);
             clone.Setup(dir, transform.position, speed);
-
             dodgeballs -= 1;
         }
     }
