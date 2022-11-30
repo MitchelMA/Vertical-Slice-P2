24855b94d1e45dd35ea59ab2437580a48944cab9
Dylan33431 <89254356+Dylan33431@users.noreply.github.com>
2022-11-30 10:30:31 +0100

    simple walking script
    Co-Authored-By: MitchelMA <90691216+MitchelMA@users.noreply.github.com>

--- Assets/Scripts/Walking.cs
+++ Assets/Scripts/Walking.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class Walking : MonoBehaviour
 {
-    [SerializeField] private Rigidbody _rb;
-    [SerializeField] private float _speed = 5;
-    private Vector2 input;
+    [SerializeField] private Rigidbody rb;
+    [SerializeField] private float speed = 5;
+    private Vector2 _input = Vector2.zero;
 
     private void Update()
     {
+        HandleInput();
+        var moveVec = speed * Time.deltaTime * _input;
 
+        transform.Translate(new Vector3(moveVec.x, 0, moveVec.y));
     }
 
 
+    private void HandleInput()
+    {
+        var inp = Vector2.zero;
+        inp.x = Input.GetAxisRaw("Horizontal");
+        inp.y = Input.GetAxisRaw("Vertical");
+        _input = inp.normalized;
+    }
 }
 
