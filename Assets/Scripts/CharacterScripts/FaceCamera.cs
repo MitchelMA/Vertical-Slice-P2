c9f16de3237ac4c1a834fa07c2576f3b50c15a90
MitchelMA <32548@ma-web.nl>
2022-12-05 11:44:19 +0100

    sets the offset and the angle to face the camera

--- Assets/Scripts/CharacterScripts/FaceCamera.cs
+++ Assets/Scripts/CharacterScripts/FaceCamera.cs
@@ -6,6 +6,9 @@ public class FaceCamera : MonoBehaviour
 {
     [SerializeField] private Camera facingCamera;
 
+    private float _alphaAngle = 0;
+    private float _betaAngle = 0;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,25 +17,34 @@ public class FaceCamera : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        TowardsCam();
+        Vector2 offset = PosOffset();
+        transform.localPosition = new Vector3(0, offset.y, offset.x);
+        transform.eulerAngles = new Vector3(-_betaAngle * Mathf.Rad2Deg+90, 0, 0);
     }
 
     // Returns the alpha angle in radians
-    private float AlphaAngleFromCam()
+    private void SetAlphaAngleFromCam()
     {
         Vector3 camForward = facingCamera.transform.forward;
         Vector3 vectorC = camForward.RotatedBy(-90 * Mathf.Deg2Rad, 'x');
 
-        return Vector3.Angle(vectorC, Vector3.up) * Mathf.Deg2Rad;
+        _alphaAngle = Vector3.Angle(vectorC, Vector3.up) * Mathf.Deg2Rad;
     }
-
-    private void TowardsCam()
+    
+    private Vector2 PosOffset()
     {
-        float alphaAngle = AlphaAngleFromCam();
-        // since the Gamma-angle is 90°, we can deduce that the beta angle is 90 - the Alpha-angle
-        float betaAngle = 90 - alphaAngle;
+        SetAlphaAngleFromCam();
+        const float pi12 = 1.570796f; 
+        // Since the Gamma-angle is 90°, we can deduce that the beta angle is 90 - the Alpha-angle
+        _betaAngle = pi12 - _alphaAngle;
+        // print($"alpha: {alphaAngle * Mathf.Rad2Deg}; beta: {betaAngle * Mathf.Rad2Deg}");
 
-        float height = transform.localScale.y;
+        float sideB = transform.localScale.y;
 
+        // Using the sinus-rule
+        float hypotenuse = sideB / Mathf.Sin(_betaAngle);
+        float moveDist = sideB - hypotenuse;
+        
+        return new Vector2(Mathf.Cos(_betaAngle) * moveDist, Mathf.Sin(_betaAngle) * moveDist);
     }
 }
