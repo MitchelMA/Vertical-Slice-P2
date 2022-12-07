4d23927028fcb62dc9de136894b24c42671e35e7
Tijmen <34179@ma-web.nl>
2022-12-07 11:15:22 +0100

    added asset, added stable swapscript

--- Assets/Scripts/charChange/changeUI.cs
+++ Assets/Scripts/charChange/changeUI.cs
@@ -5,62 +5,68 @@ using UnityEngine.UI;
 
 public class changeUI : MonoBehaviour
 {
-    public Image player1Long;
-    public Image player2Long;
-    public Image player3Long;
-    public Image player1;
-    public Image player2;
-    public Image player3;
-    
-    public float speed = 10;
-    // Start is called before the first frame update
-    void Start()
-    {
-    }
-    IEnumerator changePlayer1()
-    {
-        player1Long.transform.Translate(new Vector3(0,-2,0) * speed * Time.deltaTime);
-        yield return new WaitForSeconds(0.5f);
-        player2Long.transform.Translate(new Vector3(0,-2,0) * speed * Time.deltaTime);
-    }
-     IEnumerator changePlayer2()
-    {
-        player2Long.transform.Translate(new Vector3(0,2,0) * speed * Time.deltaTime);
-        yield return new WaitForSeconds(0.5f);
-        player1Long.transform.Translate(new Vector3(0,-2,0) * speed * Time.deltaTime);
-    }
-     IEnumerator changePlayer3()
-    {
-        player3Long.transform.Translate(new Vector3(0,2,0) * speed * Time.deltaTime);
-        yield return new WaitForSeconds(0.5f);
-        player1Long.transform.Translate(new Vector3(0,-2,0) * speed * Time.deltaTime);
-    }
-
+    public RectTransform player1Long;
+    public RectTransform player2Long;
+    public RectTransform player3Long;
+    public RectTransform player1;
+    public RectTransform player2;
+    public RectTransform player3;
+    public float speed = 0;
+    bool status1 = false;
+    bool status2 = false;
+    bool status3 = false;
     // Update is called once per frame
     void Update()
     {
-       if(Input.GetKey("1")){
-            StartCoroutine(changePlayer1());
+       if(Input.GetKeyDown("1")){
+            changePlayer1();
         }
-        if(Input.GetKey("2")){
-            StartCoroutine(changePlayer2());
+        if(Input.GetKeyDown("2")){
+            changePlayer2();
         }
-        if(Input.GetKey("3")){
-            StartCoroutine(changePlayer3());
+        if(Input.GetKeyDown("3")){
+            changePlayer3();
         }
     }
-    // void changePlayer1(){
-    // }
-    // void changePlayer2(){
-    //     player1Long.transform.Translate(Vector3.up * speed * Time.deltaTime);
-    //     player2Long.transform.Translate(Vector3.down * speed * Time.deltaTime);
-    //     player1.transform.Translate(Vector3.up * speed * Time.deltaTime);
-    //     player1.transform.Translate(Vector3.down * speed * Time.deltaTime);
-    // }
-    // void changePlayer3(){
-    //     player1Long.transform.Translate(Vector3.up * speed * Time.deltaTime);
-    //     player2Long.transform.Translate(Vector3.down * speed * Time.deltaTime);
-    //     player1.transform.Translate(Vector3.up * speed * Time.deltaTime);
-    //     player1.transform.Translate(Vector3.down * speed * Time.deltaTime);
-    // }
+      void changePlayer1(){
+        if(status1 == false){
+            status2 = false;
+            status3 = false;
+            LeanTween.move(player2Long, new Vector3(630,610,0f), speed);
+            LeanTween.move(player3Long, new Vector3(630,610,0f), speed);
+            LeanTween.move(player1Long, new Vector3(630,450,0f), speed).setDelay(0.5f);
+            LeanTween.move(player1, new Vector3(530,-610,0f), speed);
+            LeanTween.move(player3, new Vector3(760,-450,0f), speed).setDelay(0.5f);
+            LeanTween.move(player2, new Vector3(530,-450,0f), speed).setDelay(0.5f);
+        }
+       status1 = true;
+    }
+    void changePlayer2(){
+        if(status2 == false){
+            status3 = false;
+            status1 = false;
+            LeanTween.move(player3Long, new Vector3(630,610,0f), speed);
+            LeanTween.move(player1Long, new Vector3(630,610,0f), speed);
+            LeanTween.move(player2Long, new Vector3(630,450,0f), speed).setDelay(0.5f);
+            LeanTween.move(player3, new Vector3(760,-450,0f), speed).setDelay(0.5f);
+            LeanTween.move(player2, new Vector3(530,-610,0f), speed);
+            LeanTween.move(player1, new Vector3(530,-450,0f), speed).setDelay(0.5f);
+        }
+        status2 = true;
+    }
+    void changePlayer3(){
+        if(status3 == false){
+            status1 = false;
+            status2 = false;
+            LeanTween.move(player1Long, new Vector3(630,610,0f), speed);
+            LeanTween.move(player2Long, new Vector3(630,610,0f), speed);
+            LeanTween.move(player3Long, new Vector3(630,450,0f), speed).setDelay(0.5f);
+            LeanTween.move(player1, new Vector3(530,-610,0f), speed);   
+            LeanTween.move(player1, new Vector3(760,-610,0f), speed);
+            LeanTween.move(player2, new Vector3(530,-450,0f), speed).setDelay(0.5f);
+            LeanTween.move(player3, new Vector3(760,-610,0f), speed);
+            LeanTween.move(player1, new Vector3(760,-450,0f), speed).setDelay(0.5f);
+        }
+       status3 = true;
+    }
 }
