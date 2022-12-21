94ac05819c39a0d95f9d3e663fdb741303096457
stokbrootje <leonkruijer@outlook.com>
2022-12-21 12:08:02 +0100

    AutoShoot ChargedDodgeball
    Co-Authored-By: MitchelMA <90691216+MitchelMA@users.noreply.github.com>

--- Assets/Scripts/shooting/shootDodgeball.cs
+++ Assets/Scripts/shooting/shootDodgeball.cs
@@ -10,7 +10,21 @@ public class shootDodgeball : MonoBehaviour
     public float speed;
     public int dodgeballs;
     public int TargetIndex = 0;
-    public float ChargeTimer;
+    public float ChargedValue = 1f;
+
+    private float _chargeTimer = 0f;
+    public float ChargeTimer { 
+        get => _chargeTimer;
+        private set 
+        {
+            _chargeTimer = value;
+            if(_chargeTimer > ChargedValue)
+            {
+                // code to automatically shoot after charging for 1s
+                Shoot(true);
+            }
+        } 
+    }
     private bool _isCharging = false;
 
     public TextMeshProUGUI Counter;
@@ -42,12 +56,9 @@ public class shootDodgeball : MonoBehaviour
             _isCharging = value;
 
             // if prev is *true*, _isCharging is automatically *false*
-            if (prev == true)
+            if (prev == true && ChargeTimer <= ChargedValue)
             {
-                Shoot();
-                // reset charge timer
-                ChargeTimer = 0;
-                walking._speed = 5f;
+                Shoot(false);
             }
         }
     }
@@ -90,16 +101,19 @@ public class shootDodgeball : MonoBehaviour
             ChargeTimer += Time.deltaTime;
     }
 
-    private (float, Dodgeball) GetDodgeBall()
+    private (float, Dodgeball) GetDodgeBall(bool fromAutoCharged = false)
     {
-        if (ChargeTimer > 1f)
+        if (fromAutoCharged)
             return (2, ChargedDodgeball);
 
         else return (1, dodgeball);
     }
 
-    private void Shoot()
+    private void Shoot(bool fromAutoCharged = false)
     {
+        if(fromAutoCharged)
+            _isCharging = false;
+
         if (dodgeballs <= 0)
             return;
 
@@ -108,9 +122,12 @@ public class shootDodgeball : MonoBehaviour
         print(dir);
 
         // setup the clone
-        var (speedMult, ball) = GetDodgeBall();
+        var (speedMult, ball) = GetDodgeBall(fromAutoCharged);
         var clone = Instantiate(ball);
-        clone.Setup(dir, transform.position + transform.right, speed * speedMult); 
+        clone.Setup(dir, transform.position + transform.right, speed * speedMult);
+        // reset charge timer
+        ChargeTimer = 0;
+        walking._speed = 5f;
 
         // subtract from the dodgeballs
         dodgeballs -= 1;
