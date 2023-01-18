f1d152b2320e2a2b31f4634094a396d81c356905
Tijmen <34179@ma-web.nl>
2023-01-18 10:08:22 +0100

    stable swap script

--- Assets/Scripts/UI/changeUI.cs
+++ Assets/Scripts/UI/changeUI.cs
@@ -9,16 +9,16 @@ public class changeUI : MonoBehaviour
     public RectTransform player2Long;
     public RectTransform player1;
     public RectTransform player2;
-    public float speed = 0;
+    float speed = 0.3f;
     bool status1 = false;
     bool status2 = false;
  
     public void changePlayer1(){
         if(status1 == false){
             status2 = false;
-            LeanTween.move(player2Long, new Vector3(630,610,0f), speed);
+            LeanTween.move(player2Long, new Vector3(630,640,0f), speed);
             LeanTween.move(player1Long, new Vector3(630,450,0f), speed).setDelay(0.5f);
-            LeanTween.move(player1, new Vector3(530,-610,0f), speed);
+            LeanTween.move(player1, new Vector3(530,-645,0f), speed);
             LeanTween.move(player2, new Vector3(530,-450,0f), speed).setDelay(0.5f);
         }
         status1 = true;
@@ -26,9 +26,9 @@ public class changeUI : MonoBehaviour
     public void changePlayer2(){
         if(status2 == false){
             status1 = false;
-            LeanTween.move(player1Long, new Vector3(630,610,0f), speed);
+            LeanTween.move(player1Long, new Vector3(630,640,0f), speed);
             LeanTween.move(player2Long, new Vector3(630,450,0f), speed).setDelay(0.5f);
-            LeanTween.move(player2, new Vector3(530,-610,0f), speed);
+            LeanTween.move(player2, new Vector3(530,-655,0f), speed);
             LeanTween.move(player1, new Vector3(530,-450,0f), speed).setDelay(0.5f);
         }
         status2 = true;
