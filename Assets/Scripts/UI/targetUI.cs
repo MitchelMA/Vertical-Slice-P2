8a7e889d23cd421902a87d6e4d74e7353c22e6ee
Tijmen <34179@ma-web.nl>
2023-01-23 10:58:54 +0100

    should work

--- Assets/Scripts/UI/targetUI.cs
+++ Assets/Scripts/UI/targetUI.cs
@@ -7,12 +7,13 @@ public class targetUI : MonoBehaviour
     public Transform eneTwo;
     private Camera MainCamera;
     public GameObject targetIndic;
+    public GameObject targetIndic1;
     public Renderer eneRenderOne;
     public Renderer eneRenderTwo;
     public GameObject ene1Shadow;
     public GameObject ene2Shadow;
-    public float blinkTime;
-    public bool isBlinking;
+    private float blinkTime;
+    private bool isBlinking;
     public changeUI changeUI;
     
 
@@ -21,54 +22,68 @@ public class targetUI : MonoBehaviour
         MainCamera = Camera.main;
     }
     void Update(){
-        transform.Rotate(new Vector3(0, 0, -0.5f));
+        //Continuous rotation on target_indicators
+        targetIndic.transform.Rotate(new Vector3(0, 0, -0.5f));
+        targetIndic1.transform.Rotate(new Vector3(0, 0, -0.5f));
     }
-    public void track(int x){
-        if(x == 0){
-            changeUI.changePlayer1();
-            isBlinking = true;
+    public void track(int enemy){
+    //Selects a enemy
+        if(enemy == 0){
+        //First enemy
             ene1Shadow.SetActive(true);
-            StartCoroutine(BlinkChar(x));
+            ene2Shadow.SetActive(false);
             targetIndic.SetActive(true);
-            var screenPos = MainCamera.WorldToScreenPoint(eneOne.position);
-            transform.position = screenPos;
-            StartCoroutine(delay(x));
-        }
-        else if(x == 1){
-            changeUI.changePlayer2();
             isBlinking = true;
+
+            var screenPos = MainCamera.WorldToScreenPoint(eneOne.position);
+            targetIndic.transform.position = screenPos;
+            targetIndic1.transform.position = new Vector3(9999, 9999, 9999);
+            changeUI.changePlayer1();
+            StartCoroutine(delay(enemy));
+            StartCoroutine(BlinkChar(enemy));
+        }
+        else if(enemy == 1){
+        //Second enemy
+            ene1Shadow.SetActive(false);
             ene2Shadow.SetActive(true);
-            StartCoroutine(BlinkChar(x));
-            targetIndic.SetActive(true);
+            targetIndic1.SetActive(true);
+            isBlinking = true;
+            
             var screenPos = MainCamera.WorldToScreenPoint(eneTwo.position);
-            transform.position = screenPos;
-            StartCoroutine(delay(x));
+            targetIndic1.transform.position = screenPos;
+            targetIndic.transform.position = new Vector3(9999, 9999, 9999);
+            changeUI.changePlayer2();
+            StartCoroutine(delay(enemy));
+            StartCoroutine(BlinkChar(enemy));
         }
     }
-    IEnumerator delay(int y){
+    IEnumerator delay(int enemy){
+    //Hide the indicator_ui_elements after 1.5sec
         yield return new WaitForSeconds(1.5f);
-        if(y == 0){
+        if(enemy == 0){
             isBlinking = false;
             eneRenderOne.material.SetColor("_Color", new Color(1, 1, 1, 1));
-            ene1Shadow.SetActive(false);
         }   
-        else{
+        else if(enemy == 1){
             isBlinking = false;
             eneRenderTwo.material.SetColor("_Color", new Color(1, 1, 1, 1));
-            ene2Shadow.SetActive(false);
         }
+        //Moves target_indicators out of view after 1.5sec
         targetIndic.transform.position = new Vector3(9999, 9999, 9999);
+        targetIndic1.transform.position = new Vector3(9999, 9999, 9999);
     }
-    IEnumerator BlinkChar(int check) {
+    IEnumerator BlinkChar(int enemy) {
+    //Blinking, Switches enemy sprite between its default color and white
         blinkTime = 0.2f;
         while(isBlinking == true){
-            if(check == 0){
+            //Checks which enemy is selected and makes it blink 
+            if(enemy == 0){
                 eneRenderOne.material.SetColor("_Color", new Color(50, 50, 50, 0.5f));
                 yield return new WaitForSeconds(blinkTime);
                 eneRenderOne.material.SetColor("_Color", new Color(1, 1, 1, 1));
                 yield return new WaitForSeconds(blinkTime);
             }
-            else if(check == 1){
+            else if(enemy == 1){
                 eneRenderTwo.material.SetColor("_Color", new Color(50, 50, 50, 0.5f));
                 yield return new WaitForSeconds(blinkTime);
                 eneRenderTwo.material.SetColor("_Color", new Color(1, 1, 1, 1));
